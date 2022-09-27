b722eb9fd9989a12651cfe429e39290612e5e1b1
lmartin214@yahoo.com <you@example.com>
2022-09-26 18:48:19 -0700

    added appt feature able to store in local storage

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -1,2 +1,23 @@
-//adding the display of date on top of page
-$('#currentDay').text(moment().format('dddd, MMMM Do'))
+//add event listener for save button clicks
+$(document).ready(function() {
+    $(".saveBtn").click(function(){ 
+        //created 
+        let time = $(this).parent().attr("id");
+        let task =  $(this).siblings('.description').val();
+        localStorage.setItem(time, task);
+    });
+
+    // load any saved data from localStorage
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+    //adding the display of date on top of page with moment.js
+    $('#currentDay').text(moment().format('dddd, MMMM Do'))
+});
